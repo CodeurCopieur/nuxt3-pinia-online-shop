e8faef270fbb67974e7fae0a229293930cbd6883
Widdy LOUIS <louisws@outlook.fr>
2023-10-10 15:47:29 +0200

    Update Header.vue

--- components/Header.vue
+++ components/Header.vue
@@ -1,3 +1,21 @@
+<script setup>
+  const route = useRoute();
+  const router = useRouter();
+</script>
 <template>
- <h1>Header</h1>
+  <header class="shadow-sm bg-white">
+    <nav class="container mx-auto p-4 flex justify-between items-center">
+      <NuxtLink to="/" class="font-bold">CodeurCopieur</NuxtLink>
+      <ul class="flex gap-4 items-center">
+        <li><NuxtLink to="/"><i class="fa-solid fa-house"></i></NuxtLink></li>
+        <li><NuxtLink to="/cart"><i class="fa-solid fa-cart-shopping"></i></NuxtLink></li>
+        <li class="cursor-pointer btn">
+          <i v-if="theme === 'light'" 
+            class="fa-solid fa-sun"></i>
+          <i v-else class="fa-solid fa-moon"></i>
+          <span class="ml-2">Toogle theme</span>
+        </li>
+      </ul>
+    </nav>
+  </header>
 </template>
