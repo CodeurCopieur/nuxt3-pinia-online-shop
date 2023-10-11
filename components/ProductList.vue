fa2acd77afcbfd802c3f918edca87a62d372a8b1
Widdy LOUIS <louisws@outlook.fr>
2023-10-11 23:07:59 +0200

    Update ProductList.vue

--- components/ProductList.vue
+++ components/ProductList.vue
@@ -37,12 +37,16 @@
         :class="{'flex-row items-center border border-grey-dark transition-shadow hover:shadow-lg': !grid, 'flex-col': grid, 'mb-4': product !== products.length-1 && !grid}">
           <div class="relative flex items-center justify-center rounded"
             :class="{'w-full sm:w-2/5 lg:w-3/11': !grid}">
-            <div class="h-68 w-full bg-cover bg-center bg-no-repeat" style="background-image:url(https://elyssi.redpixelthemes.com/assets/img/unlicensed/sunglass-1.png)"></div>
+            <div class="h-68 w-full bg-cover bg-center bg-no-repeat w-full {bg-cover bg-center}" 
+            :style="`background-image:url(${product.thumbnail})`"></div>
             <div class="absolute inset-0 flex items-center justify-center bg-emerald-500 bg-opacity-85 py-28 opacity-0 transition-opacity hover:opacity-100">
-              <button class="mr-3 flex items-center rounded-full bg-white px-3 py-3 transition-shadow hover:shadow-lg transition-all hover:text-white hover:bg-emerald-500">
+              <button 
+                class="mr-3 flex items-center rounded-full bg-white px-3 py-3 transition-shadow hover:shadow-lg transition-all hover:text-white hover:bg-emerald-500">
                 <i class="fa-solid fa-cart-shopping"></i>
               </button>
-              <NuxtLink to="/"  class="flex items-center rounded-full bg-white px-3 py-3 transition-shadow hover:shadow-lg transition-all hover:text-white hover:bg-emerald-500">
+              <NuxtLink 
+                :to="{ path: `/product/${product.id}`}"  
+                class="flex items-center rounded-full bg-white px-3 py-3 transition-shadow hover:shadow-lg transition-all hover:text-white hover:bg-emerald-500">
                 <i class="fa-solid fa-magnifying-glass"></i>
               </NuxtLink>
             </div>
@@ -50,39 +54,36 @@
           <div v-if="grid" 
             class="flex items-center justify-between pt-6">
             <div>
-              <h3 class="font-hk text-base text-secondary">Cat eye</h3>
+              <h3 class="text-base ">{{ product.title }}</h3>
               <div class="flex items-center">
                 <div class="flex items-center">
-                  <i class="fa-solid fa-star"></i>
-                  <i class="fa-solid fa-star"></i>
-                  <i class="fa-solid fa-star"></i>
-                  <i class="fa-solid fa-star"></i>
-                  <i class="fa-solid fa-star"></i>
+                  <i 
+                    v-for="i in 5" :key="i" 
+                    class="fa-solid fa-star"
+                    :class="{'text-emerald-500': true, 'opacity-50' : i > Math.floor(product.rating)}"></i>
                 </div>
-                <p class="ml-2 font-hk text-sm text-secondary">(45)</p>
+                <p class="ml-2 text-sm ">{{ Math.floor(product.rating) }}</p>
               </div>
             </div>
-            <span class="font-hk text-xl font-bold text-primary">$75.0</span>
+            <span class="text-xl font-bold text-primary">${{ product.price }}</span>
           </div>
           <div v-else
           class="w-full px-4 lg:px-10 sm:w-3/5 sm:py-0 lg:w-8/11">
-          <h3 class="font-bold text-xl text-grey-darkest xl:text-2xl">
-            Cat eye
-          </h3>
-          <span class="block pt-1 font-hk text-xl font-bold text-secondary">$75.0</span>
-          <span class="block pt-4 font-hk text-base font-bold text-v-green">In Stock</span>
-          <p class="pt-2 font-hk text-sm text-grey-darkest xl:text-base">
-            Elyssi sunglasses provides a new way to protect your eyes from the sun’s UV light, because of its polarized lenses.
+          <h3 class="font-bold text-xl text-grey-darkest xl:text-2xl">{{ product.title }}</h3>
+          <span class="pt-1 text-xl font-bold ">${{ product.price }}</span>
+          <span class="block pt-4 text-base font-bold text-emerald-600" v-if="product.stock > 1">In Stock</span>
+          <span v-else class="font-bold pl-4 text-red-600">Pas de stock</span>
+          <p class="pt-2 text-sm text-grey-darkest xl:text-base">
+            {{  product.description }}
           </p>
           <div class="flex items-center pt-3 xl:pt-5">
             <div class="flex items-center">
-              <i class="bx bxs-star text-primary"></i>
-              <i class="bx bxs-star text-primary"></i>
-              <i class="bx bxs-star text-primary"></i>
-              <i class="bx bxs-star text-primary"></i>
-              <i class="bx bxs-star text-primary"></i>
+              <i 
+                v-for="i in 5" :key="i" 
+                class="fa-solid fa-star"
+                :class="{'text-emerald-500': true, 'opacity-50' : i > Math.floor(product.rating)}"></i>
             </div>
-            <p class="ml-2 font-hk text-sm text-secondary">
+            <p class="ml-2 text-sm ">
               45
             </p>
           </div>
